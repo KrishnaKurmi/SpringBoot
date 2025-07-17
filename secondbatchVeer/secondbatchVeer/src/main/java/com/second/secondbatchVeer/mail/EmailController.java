ea5139e4a308a482ad8e229e6f450ff943f5c03d
Krishna Kurmi <hollybuddy.ts2206@gmail.com>
2025-07-17 22:35:16 +0530

    Update EmailController.java

--- secondbatchVeer/secondbatchVeer/src/main/java/com/second/secondbatchVeer/mail/EmailController.java
+++ secondbatchVeer/secondbatchVeer/src/main/java/com/second/secondbatchVeer/mail/EmailController.java
@@ -3,6 +3,7 @@ package com.vkr.SampleProject.mail;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
