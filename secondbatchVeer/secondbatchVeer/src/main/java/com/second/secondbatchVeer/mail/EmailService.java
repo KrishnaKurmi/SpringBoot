f7798bb44721aff5ebb1de9fad5ecc75e27d06eb
Krishna Kurmi <hollybuddy.ts2206@gmail.com>
2025-07-18 14:07:05 +0530

    Update EmailService.java

--- secondbatchVeer/secondbatchVeer/src/main/java/com/second/secondbatchVeer/mail/EmailService.java
+++ secondbatchVeer/secondbatchVeer/src/main/java/com/second/secondbatchVeer/mail/EmailService.java
@@ -1,8 +1,9 @@
 package com.vkr.SampleProject.mail;
-//Frameworks
-import jakarta.mail.MessagingException;
+//Frameworksimport jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
